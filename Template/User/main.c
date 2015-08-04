4d1fd858b0917703fc3b618008d56ae51e415d7d
YuanShuai <good1138862329@gmail.com>
2015-08-04 08:16:48 +0800

    add for loop

--- Template/User/main.c
+++ Template/User/main.c
@@ -5,13 +5,15 @@
 
 int main(void)
 {		
+    int i=0;
 	delay_init();	    	 //延时函数初始化	  
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
 	uart_init(115200);	 //串口初始化为115200
  
  	while(1)
 	{
-		
+		for(i = 0; i<100;i++)
+            delay_ms(200);
 	}	 
 }
 
